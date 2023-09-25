1f5073af9637547a88439c9eb065f9c2d31eb1f9
perseus <paradox.tornio@gmail.com>
2023-09-25 19:02:49 +0300

    0.2

--- algorithms.py
+++ algorithms.py
@@ -18,10 +18,20 @@
 # if '756352' in set_:
 #     print(datetime.datetime.now() - time_now)
 
+# list_ = [9, 8, 7, 6, 5, 4, 3, 2, 1]
+# len_ = len(list_)
+# for j in range(len_):
+#     for i in range(0, len_ - 1 - j):
+#         if list_[i] > list_[i + 1]:
+#             list_[i], list_[i + 1] = list_[i + 1], list_[i]
+# print(list_)
+
 list_ = [9, 8, 7, 6, 5, 4, 3, 2, 1]
 len_ = len(list_)
-for j in range(len_):
-    for i in range(0, len_ - 1 - j):
-        if list_[i] > list_[i + 1]:
-            list_[i], list_[i + 1] = list_[i + 1], list_[i]
+for i in range(len_):
+    min_index = i
+    for j in range(i + 1, len_):
+        if list_[j] < list_[min_index]:
+            min_index = j
+    list_[i], list_[min_index] = list_[min_index], list_[i]
 print(list_)
