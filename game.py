0bfa2fd9ad56adedefd769d200c189420da43b97
perseus <paradox.tornio@gmail.com>
2023-10-15 22:34:54 +0300

    1.6

--- game.py
+++ game.py
@@ -1,10 +1,19 @@
-import time
 from typing import Union, List
 import arcade
 import random
 import pickle
-from sprites import Tile
 from config import *
+
+
+class Tile(arcade.Sprite):
+    def __init__(self, value, center_y, center_x):
+        super().__init__(f'images/num{value}.png', center_x=center_x, center_y=center_y)
+        self.direction = None
+        self.value = value
+
+    def change_value(self):
+        self.value *= 2
+        self.texture = arcade.load_texture(f'images/num{self.value}.png')
 
 
 class GameView(arcade.View):
@@ -18,13 +27,17 @@ class GameView(arcade.View):
         self.cord_dict_y = {0: 482, 1: 345, 2: 208, 3: 71}
         self.value_list = [2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096, 8192, 16384, 32768, 65536]
         self.score = 0
+        self.max_score = 0
         self.level = 2
         self.quit_game_counter = 0
+        self.is_game_over = False
 
     def on_show_view(self):
         self.setup()
 
     def setup(self):
+        self.score = 0
+        self.max_score = 0
         self.background = arcade.load_texture('images/background.png')
         self.tiles_sprite_list = arcade.SpriteList()
         self.game_field: List[List[Union[int, Tile]]] = [
@@ -37,10 +50,23 @@ class GameView(arcade.View):
         self.random_tile()
         self.random_tile()
 
+    def change_max_score(self):
+        pass
+
+    def save_game(self):
+        data = {}
+        if not self.is_game_over:
+            data['score'] = self.score
+            data['game_field'] = self.game_field
+        data['max_score'] = self.score if self.score > self.max_score else self.max_score
+        with open('game.save', 'wb') as s_file:
+            pickle.dump(self.game_field, s_file)
+
     def on_draw(self):
         self.clear()
         arcade.draw_lrwh_rectangle_textured(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT, self.background)
         arcade.draw_text(str(self.score), 90, SCREEN_HEIGHT - 33, arcade.color.BLACK, 16, 50, font_name='Arial')
+        arcade.draw_text(str(self.max_score), 450, SCREEN_HEIGHT - 33, arcade.color.BLACK, 16, 50, font_name='Arial')
         if self.score >= self.level * 500:
             self.level += 1
         for i in self.game_field:
@@ -48,6 +74,7 @@ class GameView(arcade.View):
                 if j:
                     self.quit_game_counter += 1
         if self.quit_game_counter == 16:
+            self.save_game()
             self.window.show_view(GameOverView())
         else:
             self.quit_game_counter = 0
@@ -161,6 +188,9 @@ class GameView(arcade.View):
                         self.move_right(y, x)
             self.random_tile()
 
+        if key == arcade.key.S:
+            self.save_game()
+
     def on_mouse_motion(self, x, y, delta_x, delta_y):
         pass
 
@@ -169,6 +199,29 @@ class GameView(arcade.View):
 
     def on_mouse_release(self, x, y, button, key_modifiers):
         pass
+
+
+class StartGameView(arcade.View):
+    def __init__(self):
+        super().__init__()
+        self.screen = arcade.load_texture('images/start game screen.png')
+        self.load_game_button = arcade.load_texture('images/load a game button.png')
+        self.start_game_button = arcade.load_texture('images/start a new game button.png')
+
+    def on_draw(self):
+        self.clear()
+        arcade.draw_lrwh_rectangle_textured(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT, self.screen)
+        arcade.draw_lrwh_rectangle_textured(1, (SCREEN_HEIGHT // 2 - self.load_game_button.height // 2) - 3,
+                                            self.load_game_button.width, self.load_game_button.height,
+                                            self.load_game_button)
+        arcade.draw_lrwh_rectangle_textured(1, (SCREEN_HEIGHT // 2 + self.start_game_button.height // 2) + 3,
+                                            self.start_game_button.width, self.start_game_button.height,
+                                            self.start_game_button)
+
+    def on_mouse_press(self, x, y, button, key_modifiers):
+        if button == 1 and (x >= 1 and x <= 552) and (
+                y >= 332 and y <= 388):
+            self.window.show_view(GameView())
 
 
 class GameOverView(arcade.View):
@@ -201,10 +254,11 @@ class GameOverView(arcade.View):
             elif not self.choice:
                 self.window.close()
 
+
 def main():
     """ Main function """
     window = arcade.Window(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
-    game_view = GameView()
+    game_view = StartGameView()
     window.show_view(game_view)
     arcade.run()
 
---
+++ get_exe.py
@@ -0,0 +1,14 @@
+import cx_Freeze
+
+
+
+executables = [cx_Freeze.Executable('game.py',
+                                    base='Win32GUI',
+                                    icon='images/icon.ico',
+                                    shortcut_dir='DesktopFolder',
+                                    shortcut_name='2048')]
+cx_Freeze.setup(
+    name='2048', options={'build_exe': {'excludes':['tkinter'], 'packages': ['arcade'],
+                                           'include_files': ['images']},
+                          "bdist_msi": {'target_name': '2048.msi', 'install_icon': 'images/icon.ico'}},
+    executables=executables)
